6cc69929f3f7f18bd6f0e85037ad5eb4c264ade7
Josh <joshmoo2009@hotmail.com>
2018-03-16 17:06:29 +0000

    Modified the run command for the api

--- run.py
+++ run.py
@@ -5,11 +5,23 @@
 import os
 import subprocess
 
-# start api
+# Global Varibles
 PROJECT_DIR = "/home/pi/Project"
-API = "PiesuesAPI"
-api_dir = os.path.join([PROJECT_DIR, API])
+LOG_DIR = "/home/pi/Project/_logs"
+LOG_FILE_NAME = "log.%s.txt"
+NOHUP_CMD = "nohup %s &"
+PYTHON_CMD = "python %s > %s"
 
+# Construct API start command
+API = "PiesuesAPI"
+api_dir = os.path.join(PROJECT_DIR, API)
+log_dir = os.path.join(LOG_DIR, LOG_FILE_NAME % ('api'))
+python_cmd = PYTHON_CMD % ('rest_api.py', log_dir)
+api_cmd = NOHUP_CMD % (python_cmd)
+
+# Execute API start command
+cwd = os.getcwd()
 os.chdir(api_dir)
 print('Starting API')
-status = subprocess.check_output("nohup python rest_api.py &", shell=True)
+status = subprocess.check_output(api_cmd, shell=True)
+os.chdir(cwd)
